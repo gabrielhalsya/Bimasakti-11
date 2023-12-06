7327f25ba409e27d28b664d83d962ec98ae8c349
gabrielhalsya <gabrielm1603@gmail.com>
2023-12-06 17:04:16 +0700

    lmm00200 & lmm03700 ui fit to fullscreen browser

--- BACK/LM/LM_SPR/RSP_LM_MAINTAIN_UNIT_TYPE_PRICEResources/obj/Debug/netstandard2.1/RSP_LM_MAINTAIN_UNIT_TYPE_PRICEResources.AssemblyInfo.cs
+++ BACK/LM/LM_SPR/RSP_LM_MAINTAIN_UNIT_TYPE_PRICEResources/obj/Debug/netstandard2.1/RSP_LM_MAINTAIN_UNIT_TYPE_PRICEResources.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RSP_LM_MAINTAIN_UNIT_TYPE_PRICEResources")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38132c1c18bb439380cf90dc34a4e13a7706483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7162e0180b3d06594d4010c3c9c6bbb726161e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RSP_LM_MAINTAIN_UNIT_TYPE_PRICEResources")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RSP_LM_MAINTAIN_UNIT_TYPE_PRICEResources")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SERVICE/LM/LMM00200Service/obj/Debug/net6.0/LMM00200Service.AssemblyInfo.cs
+++ SERVICE/LM/LMM00200Service/obj/Debug/net6.0/LMM00200Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMM00200Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38132c1c18bb439380cf90dc34a4e13a7706483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7162e0180b3d06594d4010c3c9c6bbb726161e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMM00200Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMM00200Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
