9ad39ea1045f7c810583dc0eac8f02c6dc72c314
gabrielhalsya <gabrielm1603@gmail.com>
2024-01-09 09:50:28 +0700

    salah versi

---
+++ COMMON/LM/LMM03700Common/LoggerLMM03700.cs
@@ -0,0 +1,12 @@
+﻿using R_Common;
+using R_CommonFrontBackAPI.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMM03700Common
+{
+    public class LoggerLMM03700 : R_NetCoreLoggerBase<LoggerLMM03700>
+    {
+    }
+}
